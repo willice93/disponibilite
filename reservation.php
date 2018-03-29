c180222d726b5f0c2640d3af4f288d5a9d373ac7
willice <willice93@hotmail.com>
2018-03-29 07:59:08 +0200

    page de resrvetion formulaire ameliorer boostrap installé

--- reservation.php
+++ reservation.php
@@ -12,31 +12,47 @@ $numseance=$_GET['seance'];
 $heure=$horaires[$numseance];
 
 
-echo 'vous desirez reserver le '.$date.' de '.$heure .'?'
+echo '<p>vous desirez reserver le '.$date.' de '.$heure .'?</p>'
 ;echo'<br>';
-echo 'vous devez vous inscrire';
+echo '';
 ?>
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<title>confirmation</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
-<body>
 
-<form action="resaMembre.php" method="POST">
+<body>
+<div class="form-group col-lg-3 ">
+<form action="resaMembre.php" method="POST" class="">
 	
-	  <input type="radio" name="x" value="oui"  ><label>oui</label> 
-  <input type="radio" name="y" value="non" ><label>non</label><br>
-	<input type="texte" name="nom"><label>nom</label></br>
-	<input type="texte" name="prenom"><label>prenom</label></br>
-	<input type="submit" name="confirmez">
+	<label>oui</label>   <input type="radio" name="x" value="oui"  class=" " >
+  <label>non</label> <input type="radio" name="x" value="non" class="" >
+
+
+  
+    <div class="input-group">
+		<span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+	<input type="texte" name="nom" class="form-control">
+	</div>
+			<br>		<div class="input-group">
+				<span class="input-group-addon"><i class="glyphicon glyphicon-envelope"></i></span>
+					<input type="texte" name="prenom" class="form-control">
+					
+					</div>
+					<br>
+					<input type="submit" name="confirmez" class="btn btn-success"">
 </form>
 <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
 <input type="hidden" name="cmd" value="_s-xclick">
 <input type="hidden" name="hosted_button_id" value="KUJP6WTPNMXAW">
 <input type="image" src="https://www.paypalobjects.com/WEBSCR-640-20110306-1/fr_FR/FR/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal, le réflexe sécurité pour payer en ligne">
 <img alt="" border="0" src="https://www.paypalobjects.com/fr_FR/i/scr/pixel.gif" width="1" height="1">
-</form>
 
+</form>
+</div>
 </body>
 </html>
